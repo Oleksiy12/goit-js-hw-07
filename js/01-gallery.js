6df31a0cd2a5f99fa6c037d536dab0f36414c4ad
Oleksiy12 <betbonnay@gmail.com>
2023-04-25 18:39:22 +0300

    update2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+const gallery__item = document.querySelector(".gallery__item")
+const allImg = galleryItems.forEach(item => {
+  
 
-const galleryItem = document.getElementById("galleryItems");
-
+})
