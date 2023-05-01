fd37a86dfe6326c23690e14808ecd19a6a6747eb
Oleksiy12 <betbonnay@gmail.com>
2023-05-01 12:12:14 +0300

    update6

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,23 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-const gallery = document.querySelector(".gallery");
+// const gallery = document.querySelector(".gallery");
 
-const galleryItem = galleryItems.map(({preview, original, description}) => 
-    `<li class = "gallery__item"><a class="gallery__link" href="${original}">
-    <img class ="gallery__image" src="${preview}" alt="${description}"/>
-    </a>
-    </li>`).join("");
+// const galleryItem = galleryItems.map(({preview, original, description}) => 
+//     `<li class = "gallery__item"><a class="gallery__link" href="${original}">
+//     <img class ="gallery__image" src="${preview}" alt="${description}"/>
+//     </a>
+//     </li>`).join("");
 
-    gallery.insertAdjacentHTML("beforeend", galleryItem);
-    gallery.addEventListener("click", onModalClick);
+//     gallery.insertAdjacentHTML("beforeend", galleryItem);
+//     gallery.addEventListener("click", onModalClick);
 
-    function onModalClick(event){
-    event.preventDefault();
-    // const currentItem = event.target;
-    // if(currentItem.nodeName !== "IMG"){
-    //     return;
-    // }
-    const lightbox = new SimpleLightbox(".gallery .gallery__link",    
-    {captionData: "alt", captionDelay: 250});
-  }
+//     function onModalClick(event){
+//     event.preventDefault()}
+    
+//     const lightbox = new SimpleLightbox(".gallery .gallery__link",    
+//     {captionsData: "alt", captionDelay: 250});
+  
